6606d721c70a7304b1a0b118f980930e99a2099d
puneet222 <aggarwal.puneet222@gmail.com>
2021-06-13 18:27:46 +0530

    22 hands on part1-1

---
+++ 022_hands-on/01/01_hands-on/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func def(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "def.gohtml", nil)
+	HandleError(w, err)
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "dog.gohtml", nil)
+	HandleError(w, err)
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Errorf("error %v", err)
+	}
+	name := r.URL.Query()
+	err = tpl.ExecuteTemplate(w, "me.gohtml", name)
+	HandleError(w, err)
+}
+
+func main() {
+	http.HandleFunc("/", def)
+	http.HandleFunc("/dog", dog)
+	http.HandleFunc("/me/", me)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
